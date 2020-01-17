850102cb972f5080d04d9315fd84c26a1c10cedf
Pavel Sanchez <pavel@bluedot.dev>
2020-01-17 18:11:25 -0500

    :art: Cleaning up code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-const zipInput = document.querySelector('#input');
-const zips = document.querySelector('#zipConverter');
-const reset = document.querySelector('#reset-button');
-
 /* ==============================
 @Zip Converter
 ============================== */
-// *Get Zips
