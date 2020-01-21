241aae6f7b596e9c388d25bd4e4691df275cb4f7
Pavel Sanchez <pavel@bluedot.dev>
2020-01-20 19:31:56 -0500

    :recycle: Clearning up code

--- src/app.js
+++ src/app.js
@@ -6,31 +6,24 @@ const input = document.getElementById('zip-input');
 const output = document.getElementById('zip-output');
 const submitBtn = document.getElementById('submit-button');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', e => {
 	e.preventDefault();
 	const inputVal = input.value;
 	const res = inputVal.split(',');
 
 	for (let i = 0; i < res.length; i++) {
-		// Trim the excess whitespace.
 		res[i] = res[i].replace(/^\s*/, '').replace(/\s*$/, '');
-		// Add additional code here, such as:
 		const convertedZips = `"${res[i]}"|`;
-		const zips = convertedZips;
 
-		output.innerHTML += zips;
+		output.innerHTML += convertedZips;
 	}
 
 	const removePipe = output.textContent.slice(0, -1);
 	output.innerHTML = removePipe;
-
-	//clear textarea on submit
 	input.value = '';
 });
 
-// FIX: Instead of having it window reload -- get this to clear input and output without reloading the page.
-form.addEventListener('reset', function(e) {
+form.addEventListener('reset', e => {
 	location.reload();
+	e.preventDefault();
 });
-
-// TODO: Add A Copy Button
